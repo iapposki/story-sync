d19567aab20e3c87426ec4b92538c7de47f85dad
iapposki <konark981999@gmail.com>
2023-12-08 00:28:21 +0530

    added test data to match personalities

--- server/controllers/postPersonality.controller.ts
+++ server/controllers/postPersonality.controller.ts
@@ -8,16 +8,23 @@ export const postPersonality = async (req: Request, res: Response) => {
     const priorityComparator = (a: any,b: any) => deviation(a,data) - deviation(b, data)
     var customHeap = new Heap(priorityComparator)
 
-    var book3 = [0,1,0,1,2, {'name': 'book3'}]
-    var book2 = [10,11,12,11,12, {'name': 'book2'}]
-    var book1 = [20,23,24,21,20, {'name': 'book1'}]
 
-    customHeap.init([book1, book2, book3])
-    console.log(customHeap.pop())
-    console.log(customHeap.pop())
-    console.log(customHeap.pop())
+    const books = [
+        [-0.17, -0.15, 0.28, 0.20, 0.18, {name: "American Psycho by Bret Easton Ellis"}],
+        [0.13, 0.20, -0.11, -0.20, -0.16, {name: "Kindred by Octavia E. Butler"}],
+        [-0.17, -0.17, -0.26, -0.24, -0.16, {name: "The Name of the Rose by Umberto Eco"}],
+        [-0.15, -0.12, -0.17, -0.16, -0.11, {name: "Dark Matter by Blake Crouch"}],
+        [0.17, 0.20, 0.25, 0.25, 0.21, {name: "The Diary of Anne Frank by Anne Frank"}],
+        [0.13, 0.12, 0.14, 0.15, 0.18, {name: "The Color Purple by Alice Walker"}]
+      ];
+    var result: any;
+    customHeap.init([books[0]]);
+    for (let i = 1; i < books.length; i++) {
+        customHeap.push(books[i])
+    }
+    result = customHeap.pop()[personalityArray.length]['name']
 
-    res.status(201).json({ranking: "OK"})
+    res.status(201).json(result)
 }
 
 const personalityArray = ['extraversion', 'agreeableness', 'openness', 'neuroticism', 'conscientiousness'];
